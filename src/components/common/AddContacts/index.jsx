319c65bea44f3946e94ca95af411049f40ffc6bf
CoderNazar <nazardubovik383@gmail.com>
2022-10-21 00:10:06 +0300

    chore: moved input in separate files

--- src/components/common/AddContacts/index.jsx
+++ src/components/common/AddContacts/index.jsx
@@ -1,32 +1,21 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { Button } from 'components/ui';
+import { Button, InputName, InputNumber } from 'components/ui';
 import { Section } from 'components/hoc';
 
 class AddContacts extends Component {
-  static defaultProps = {};
-  static propTypes = {};
+  static propTypes = {
+    onSubmit: PropTypes.func.isRequired,
+  };
   render() {
     const { onSubmit } = this.props;
     return (
       <form onSubmit={onSubmit}>
         <Section title={'Name'}>
-          <input
-            type="text"
-            name="name"
-            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-            required
-          />
+          <InputName />
         </Section>
         <Section title={'Number'}>
-          <input
-            type="tel"
-            name="number"
-            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-            required
-          />
+          <InputNumber />
         </Section>
         <Button type={'submit'} text={'Add contact'} />
       </form>
