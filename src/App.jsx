6e5503120bb5dbf626c206a8f321fc55ab107e8a
CoderNazar <nazardubovik383@gmail.com>
2022-10-21 00:10:38 +0300

    chore: add func filter

--- src/App.jsx
+++ src/App.jsx
@@ -5,28 +5,61 @@ import { AddContacts, Contacts } from 'components/common';
 class App extends Component {
   state = {
     contacts: [],
-    filter: '',
-    name: '',
-    number: '',
+    current: [],
   };
+
+  filterContacts({ target }) {
+    const value = target.value;
+    const newState = { ...this.state };
+    const { current } = newState;
+    const filteredContact = current.filter(({ name }) =>
+      name.toLowerCase().includes(value.toLowerCase())
+    );
+    this.setState({
+      ...newState,
+      contacts: filteredContact,
+    });
+  }
+
+  deleteContact(idContact) {
+    const newState = { ...this.state };
+    const { current } = newState;
+    const newContacts = current.filter(({ id }) => id !== idContact);
+    this.setState({
+      ...newState,
+      contacts: newContacts,
+      current: newContacts,
+    });
+  }
+
+  getIsSameName(nameNewContact) {
+    const { current } = { ...this.state };
+    return current.some(({ name }) => name === nameNewContact);
+  }
 
   onSubmit(event) {
     event.preventDefault();
-    const id = nanoid();
     const { name, number } = [...event.target.elements]
       .filter(item => item.name)
       .reduce(
         (item, element) => ((item[element.name] = element.value), item),
         {}
       );
-    const newState = { ...this.state };
-    const { contacts } = newState;
-    contacts.push({ name, number, id });
-    this.setState({
-      ...newState,
-      contacts,
-    });
-    event.currentTarget.reset();
+    const isSame = this.getIsSameName(name);
+    if (!isSame) {
+      const id = nanoid();
+      const newState = { ...this.state };
+      const { contacts } = newState;
+      contacts.push({ name, number, id });
+      this.setState({
+        ...newState,
+        contacts,
+        current: contacts,
+      });
+      event.currentTarget.reset();
+    } else {
+      alert(`${name} is already in contacts.`);
+    }
   }
 
   render() {
@@ -34,7 +67,11 @@ class App extends Component {
     return (
       <>
         <AddContacts onSubmit={this.onSubmit.bind(this)} />
-        <Contacts contacts={contacts} />
+        <Contacts
+          contacts={contacts}
+          filterContacts={this.filterContacts.bind(this)}
+          deleteContact={this.deleteContact.bind(this)}
+        />
       </>
     );
   }
